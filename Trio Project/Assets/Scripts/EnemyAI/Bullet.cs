edd9dc8a766284a5ef6795562ecc1e1fdd6f37b9
Samuel <Ford_977@msn.com>
2018-11-12 23:51:07 -0800

    created 3 basic simple object enemies. All have basic chase script to chase player. Sphere enemy currently has basic fire attack to instantiate block bullet at player.

---
+++ Trio Project/Assets/Scripts/EnemyAI/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+public float BulletLife = 6.0f;
+public float BulletSpeed = 50.0f;
+ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		BulletLife -= Time.deltaTime;
+		if (BulletLife <= 0) {
+		Destroy(gameObject);
+		}
+
+		if (BulletLife > 0) {
+			transform.position += transform.forward * BulletSpeed * Time.deltaTime;
+		}
+	}
+}
+
+
