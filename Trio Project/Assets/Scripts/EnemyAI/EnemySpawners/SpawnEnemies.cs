7be3906af97a615a400e3ab5a9544433240dab03
Samuel <Ford_977@msn.com>
2018-11-27 00:56:29 -0800

    added short n sweet rand spawn for spawners to spawn all 3 enemies randomely, and added back and forth movement to the spawners as well.

--- Trio Project/Assets/Scripts/EnemyAI/EnemySpawners/SpawnEnemies.cs
+++ Trio Project/Assets/Scripts/EnemyAI/EnemySpawners/SpawnEnemies.cs
@@ -6,29 +6,50 @@ public class SpawnEnemies : MonoBehaviour {
 
 public float SpawnTimer = 3.0f;
 public bool IsSpawning;
+public bool SpawnerTransition;
+public float SpawnMovementSpeed =0.05f;
+public bool SpawnMover;
+public Transform StartPosition;
+public Transform EndPosition;
 public GameObject[] EnemyTypes;
 
 
 	// Use this for initialization
-	void Start () {
-	IsSpawning = true;
+	void Start ()
+	{
+	SpawnMover = false;
+		IsSpawning = true;
 		EnemyTypes = Resources.LoadAll<GameObject> ("Prefabs/Enemies");
 		if (IsSpawning == true) {
-		StartCoroutine(BeginSpawning());
+			StartCoroutine (BeginSpawning ());
 		}
-		
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+	float step = SpawnMovementSpeed * Time.deltaTime;
+		if (SpawnMover == false) {
+			transform.position = Vector3.MoveTowards(transform.position, EndPosition.position, step);
+			if (transform.position == EndPosition.position) {
+
+				SpawnMover = true;
+			}
+		}
+
+		if (SpawnMover == true) {
+			transform.position = Vector3.MoveTowards(transform.position, StartPosition.position, step);
+			if (transform.position == StartPosition.position) {
+				SpawnMover = false;
+			}
+		}
 	}
 
 
 	IEnumerator BeginSpawning ()
 	{
 	yield return new WaitForSeconds(SpawnTimer);
-	Instantiate (EnemyTypes[2], transform.position, transform.rotation);
+	Instantiate (EnemyTypes[Random.Range(0,3)], transform.position, transform.rotation);
 	StartCoroutine(BeginSpawning());
 
 	}
