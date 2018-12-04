7566445507ba55e829bb4ef62a9b408cdb26fef1
Michael Mahung <scgmahung@gmail.com>
2018-12-04 10:42:36 -0600

    fixed player projectiles throwing errors

--- Trio Project/Assets/Scripts/Combat/PlayerWeapons/ProjectileScripts/FlamethrowerProjectile.cs
+++ Trio Project/Assets/Scripts/Combat/PlayerWeapons/ProjectileScripts/FlamethrowerProjectile.cs
@@ -7,30 +7,18 @@ public class FlamethrowerProjectile : BaseProjectile
 
     public override void OnTriggerEnter(Collider other)
     {
+
         string hitTag = other.gameObject.tag;
         IDamageable<float> thingHit = other.gameObject.GetComponent<IDamageable<float>>();
 
-        if (thingHit != null && hitTag == "Wall")
-        {
-            thingHit.Damage(projectileDamage);
-            Deactivate();
-        } 
-
         if (thingHit != null && hitTag != "Player")
         {
+            thingHit.Damage(projectileDamage);
             if (hitTag == "Wall")
             {
-                thingHit.Damage(projectileDamage);
                 Deactivate();
             }
-            try
-            {
-                thingHit.Damage(projectileDamage);
-            }
-            catch
-            {
-                Debug.LogErrorFormat("{0} does not have IDamageable interface attached.", hitTag);
-            }
+            //Deactivate();
         }
 
         else if (hitTag == "Wall")
