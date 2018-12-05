8eb35fb15b5a1a16462956ede940afc82a8edc9f
Harugot <33010386+Harugot@users.noreply.github.com>
2018-12-05 01:41:17 -0600

    Second level added based on destroying objects

---
+++ Trio Project/Assets/Scripts/Environment/BaseDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseDoor : MonoBehaviour
+{
+    public enum moveAxis { X, Y, Z }
+    public moveAxis MoveAxis;
+    public float moveAmount = 10;
+    protected Vector3 moveDirection;
+    protected bool doorMoved;
+
+
+    public void Start()
+    {
+        switch (MoveAxis)
+        {
+            case moveAxis.X:
+                moveDirection = new Vector3(moveAmount, 0, 0);
+                break;
+            case moveAxis.Y:
+                moveDirection = new Vector3(0, moveAmount, 0);
+                break;
+            case moveAxis.Z:
+                moveDirection = new Vector3(0, 0, moveAmount);
+                break;
+            default:
+                moveDirection = new Vector3(0, 0, moveAmount);
+                break;
+        }
+    }
+
+    public virtual void OpenDoor()
+    {
+        doorMoved = true;
+        transform.localPosition += moveDirection;
+    }
+}
