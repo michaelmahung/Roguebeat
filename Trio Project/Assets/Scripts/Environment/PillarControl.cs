90c341dc56a90b67234b81d7dcec3779ff8a2944
Friday <jfriday.fullsail@gmail.com>
2018-12-08 21:17:42 -0500

    working on moving pillars in JF_Scene

---
+++ Trio Project/Assets/Scripts/Environment/PillarControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarControl : MonoBehaviour {
+
+    public Transform up;
+    public Transform down;
+
+    public float moveSpeed;
+
+    public bool turnOn;
+    bool onOnce;
+
+    
+
+    // Use this for initialization
+    void Start () {
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = moveSpeed * Time.deltaTime;
+
+		if (turnOn == true && onOnce == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, up.position, move);
+            onOnce = true;
+        }
+        if(turnOn == false && onOnce == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, down.position, move);
+            onOnce = false;
+        }
+	}
+
+    void ChangeSize()
+    {
+       
+    }
+}
