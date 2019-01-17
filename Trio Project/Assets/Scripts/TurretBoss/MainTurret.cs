8482f681ab23f2f4a61858fbf894f096c45b35c1
Friday <jfriday.fullsail@gmail.com>
2019-01-16 22:15:46 -0500

    boss does slow track of player, press L (for now) to quick rotate to position, side turrets enter into phase 1 of attack

---
+++ Trio Project/Assets/Scripts/TurretBoss/MainTurret.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTurret : MonoBehaviour {
+
+    public MainController controller;
+
+    public int health;
+
+
+	// Use this for initialization
+	void Start () {
+
+        controller = gameObject.GetComponent<MainController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Trio Project/Assets/Scripts/TurretBoss/SideTurret.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideTurret : MonoBehaviour {
+
+    public MainController controller;
+    public GameObject body;
+
+    public int health;
+    public int maxHealth = 10;
+
+    public bool disabled;
+    public bool dead;
+
+    public GameObject shot1;
+    public GameObject shot2;
+    public GameObject shot3;
+
+    public float atkTime;
+
+    public GameObject spawn;
+
+    public bool p1fire;
+    public bool p2fire;
+    public bool p3fire;
+
+    public bool attacking;
+
+    // Use this for initialization
+    void Start () {
+
+        controller = body.GetComponent<MainController>();
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update () {
+		
+        if(controller.phase == "Attack")
+        {
+           
+            if(controller.attackPhase == 1)
+            {
+                atkTime = 1.0f;
+                if (attacking == false)
+                {
+                    attacking = true;
+                    StartCoroutine(PhaseOne());
+                }
+                
+            }
+
+            if (controller.attackPhase == 2)
+            {
+
+            }
+
+            if (controller.attackPhase == 3)
+            {
+
+            }
+
+
+        }
+
+	}
+
+    public IEnumerator PhaseOne()
+    {
+        yield return new WaitForSeconds(atkTime);
+        p1fire = false;
+        GameObject fire;
+        fire = Instantiate(shot1, spawn.transform.position, spawn.transform.rotation) as GameObject;
+        if(p1fire == false)
+        {
+            print("I am shooting a bullet");
+            p1fire = true;
+            StartCoroutine(PhaseOne());
+            
+        }
+        else
+        {
+            attacking = false;
+            StopCoroutine(PhaseOne());
+        }
+    }
+
+    void PhaseTwo()
+    {
+
+    }
+
+    void PhaseThree()
+    {
+
+    }
+}
