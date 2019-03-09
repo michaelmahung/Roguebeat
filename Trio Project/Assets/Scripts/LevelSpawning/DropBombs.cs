ee9aa34e9686375434137592556c25a6e0ee654b
Samuel <Ford_977@msn.com>
2019-03-09 12:11:04 -0800

    Bomb Room submission, communicates with gamemanager now to open doors.

--- Trio Project/Assets/Scripts/LevelSpawning/DropBombs.cs
+++ Trio Project/Assets/Scripts/LevelSpawning/DropBombs.cs
@@ -8,17 +8,20 @@ public class DropBombs : MonoBehaviour
     public int TotalBombs;
     public int CurrentBombs;
 
+    private RoomSetter MyRoom;
+
     public GameObject[] Bombspawners;
-	public GameObject BombPrefab;
-	private float WaitToSpawn;
+    public GameObject BombPrefab;
+    private float WaitToSpawn;
 
     //public int BombLimit;
 
     // Use this for initialization
-    void start()
+    void Start()
     {
-		CurrentBombs = 0;
-		WaitToSpawn = 0;
+        CurrentBombs = 0;
+        WaitToSpawn = 0;
+        MyRoom = GetComponent<RoomSetter>();
 
     }
 
@@ -27,19 +30,21 @@ public class DropBombs : MonoBehaviour
     {
         if (CurrentBombs >= TotalBombs)
         {
-            //print(CurrentBombs);
+            RoomManager.Instance.AddToDoor(MyRoom, RoomManager.RoomType.Timed);
+            Debug.Log("Went through");
         }
 
         else
         {
-WaitToSpawn += Time.deltaTime;
-if(WaitToSpawn >= 0.5f){
-            int random = Random.Range(0, Bombspawners.Length);
-			GameObject bomb = Instantiate(BombPrefab, Bombspawners[random].transform.position, Bombspawners[random].transform.rotation);
-            bomb.GetComponent<Rigidbody>().AddRelativeForce(transform.forward * 20000);
-			CurrentBombs++;
-			WaitToSpawn = 0;
-}
+            WaitToSpawn += Time.deltaTime;
+            if (WaitToSpawn >= 0.5f)
+            {
+                int random = Random.Range(0, Bombspawners.Length);
+                GameObject bomb = Instantiate(BombPrefab, Bombspawners[random].transform.position, Bombspawners[random].transform.rotation);
+                bomb.GetComponent<Rigidbody>().AddRelativeForce(transform.up * 20000);
+                CurrentBombs++;
+                WaitToSpawn = 0;
+            }
         }
 
     }
